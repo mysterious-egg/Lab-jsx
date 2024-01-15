e048f54a533e972ed912132c6a15b288c6ce59ba
Manvesh Thakur <manveshthakur0102@gmail.com>
2024-01-16 02:27:40 +0530

    Added

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import elephant from "./images/elephant.jpeg";
+
+const imageData = ()=>{
+  let data = [
+    {
+      id:1,
+      img:elephant
+    },
+    {
+      id:2,
+      img:elephant
+    },
+    {
+      id:3,
+      img:elephant
+    },
+    {
+      id:4,
+      img:elephant
+    }
+  ]
+  return data;
+}
+
+function App() {
+  const img = imageData();
+
+  return (
+    <>
+      {img.map(item => (
+        <img key={item.id} src={item.img} alt="" />
+      ))}
+    </>
+  );
+}
+export default App;
